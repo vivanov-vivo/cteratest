09f9c77b4466bcea74fc8cf7bd4dfe926d61adcf
Victoria Ivanov <68241162+vivanov-vivo@users.noreply.github.com>
2021-05-30 19:44:32 +0300

    Update deliver.sh
    update with the last 30.05.21

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,3 +6,13 @@ NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 java -jar target/${NAME}-${VERSION}.jar
+
+##!/usr/bin/env bash
+#set -x
+##cd cteratest
+#mvn jar:jar install:install help:evaluate -Dexpression=project.name
+#NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+#VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+#set +x
+#java -jar target/${NAME}-${VERSION}.jar
+##java -jar target/my-app-1.0-SNAPSHOT.jar
